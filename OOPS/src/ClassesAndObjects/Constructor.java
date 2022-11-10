b5943e5e0b76f8853835ae4b725d97e04677b72c
Sudhanshu Shrivastava <116457422+SudhanshuShrivastava04@users.noreply.github.com>
2022-11-10 22:44:48 +0530

    Update Constructor.java

--- OOPS/src/ClassesAndObjects/Constructor.java
+++ OOPS/src/ClassesAndObjects/Constructor.java
@@ -4,10 +4,13 @@ public class Constructor {
     public String Name;
     public final int Age;
     // final keyword is used to set the value non-mutable further
+    static int numStudents=0;
+    // static means its same from the refrence of any object 
     public Constructor(String name , int age){
         Name = name;
         if (age>=18){Age = age;}
         else Age = 0;
+        numStudents++;
     }
     public void print(){
         System.out.println(Name+ " : " +Age);
