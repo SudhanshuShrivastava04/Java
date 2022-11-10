dbfc60fcf5531879691f2e252dd3aa2e2bfb09ea
Sudhanshu Shrivastava <116457422+SudhanshuShrivastava04@users.noreply.github.com>
2022-11-10 22:56:23 +0530

    Update ConstructorUse.java

--- OOPS/src/ClassesAndObjects/ConstructorUse.java
+++ OOPS/src/ClassesAndObjects/ConstructorUse.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class ConstructorUse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Constructor c1 = new Constructor("Sudhanshu" , 19);
+        Constructor c1 = new Constructor("Sudhanshu" , 20);
         Constructor c2 = new Constructor("Ankush" , 18);
         Constructor c3 = new Constructor("Satwik" , 17);
         c1.print();
         c2.print();
         c3.print();
-        // here I consider 0 as null
+        c1.print();
+        System.out.println(c1.getNumStudents());
         Constructor c4 = new Constructor("arnav" , 19);
         c4.print();
-        System.out.println(c1.numStudents);
+        System.out.println(c1.getNumStudents());
         // here I consider 0 as null
     }
 }
